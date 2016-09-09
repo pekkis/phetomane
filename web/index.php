926e8704e0aac4c67eb0aad36375b95e9a0614e8
Mikko Forsström <puhemies@diktaattoriporssi.com>
2016-09-09 13:17:07 +0300

    Routing.

--- web/index.php
+++ web/index.php
@@ -2,35 +2,60 @@
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\RouteCollection;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $request = Request::createFromGlobals();
 
-$path = $request->getPathInfo();
+$routes = new RouteCollection();
+$routes->add(
+    'index',
+    new Route('', [
+        'who' => 'nobody',
+        '_controller' => 'hello'
+    ])
+);
+$routes->add(
+    'hello',
+    new Route('hello/{who}', [
+        'who' => 'nobody',
+        '_controller' => 'hello'
+    ])
+);
+$routes->add(
+    'goodbye',
+    new Route('goodbye', [
+        '_controller' => 'goodbye'
+    ])
+);
 
-$map = [
-    '/' => '/hello',
-    '/hello' => function(Request $request) {
-        $response = new Response();
-        $who = $request->query->get('hello', 'nobody');
-        $response->setContent(sprintf('hello, %s', $who));
-        return $response;
-    },
-    '/goodbye' => function() {
-        return Response::create('goodbye :(');
-    }
-];
+$context = new RequestContext();
+$context->fromRequest($request);
 
-if (isset($map[$path])) {
+$matcher = new UrlMatcher($routes, $context);
 
-    // Index page kludge!
-    if (is_string($map[$path])) {
-        $path = $map[$path];
-    }
+try {
+    $match = $matcher->match($request->getPathInfo());
+    $request->attributes->add($match);
 
-    $response = $map[$path]($request);
-} else {
+    $controllers = [
+        'hello' => function(Request $request) {
+            $response = new Response();
+            $who = $request->attributes->get('who');
+            $response->setContent(sprintf('hello, %s', $who));
+            return $response;
+        },
+        'goodbye' => function() {
+            return Response::create('goodbye :(');
+        }
+    ];
+    $response = $controllers[$match['_controller']]($request);
+} catch (ResourceNotFoundException $e) {
     $response = Response::create('not found')->setStatusCode(404);
 }
 
