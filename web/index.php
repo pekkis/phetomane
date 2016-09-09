fef55198b81f8e93053869bf3ef6b44c442e65dc
Mikko Forsström <puhemies@diktaattoriporssi.com>
2016-09-09 13:50:51 +0300

    HTTP kernelized.

--- web/index.php
+++ web/index.php
@@ -1,64 +1,13 @@
 <?php
 
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\RouteCollection;
-use Symfony\Component\Routing\Route;
-use Symfony\Component\Routing\RequestContext;
-use Symfony\Component\Routing\Matcher\UrlMatcher;
-use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+use Pekkis\Phetomane\Application;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $request = Request::createFromGlobals();
 
+$phetomane = new Application();
 
-function hello(Request $request) {
-    $response = new Response();
-    $who = $request->attributes->get('who');
-    $response->setContent(sprintf('hello, %s', $who));
-    return $response;
-}
-
-function goodbye() {
-    return Response::create('goodbye :(');
-}
-
-$routes = new RouteCollection();
-$routes->add(
-    'index',
-    new Route('', [
-        'who' => 'nobody',
-        '_controller' => 'hello'
-    ])
-);
-$routes->add(
-    'hello',
-    new Route('hello/{who}', [
-        'who' => 'nobody',
-        '_controller' => 'hello'
-    ])
-);
-$routes->add(
-    'goodbye',
-    new Route('goodbye', [
-        '_controller' => 'goodbye'
-    ])
-);
-
-$context = new RequestContext();
-$context->fromRequest($request);
-
-$matcher = new UrlMatcher($routes, $context);
-
-try {
-    $match = $matcher->match($request->getPathInfo());
-    $request->attributes->add($match);
-    $response = $match['_controller']($request);
-} catch (ResourceNotFoundException $e) {
-    $response = Response::create('not found')->setStatusCode(404);
-}
-
-$response
-    ->prepare($request)
-    ->send();
+$response = $phetomane->handle($request);
+$response->send();
