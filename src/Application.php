a7c2599bb8b2389c143faccf9c2b6ac6dfb6dbd1
Mikko Forsström <puhemies@diktaattoriporssi.com>
2016-09-09 14:14:34 +0300

    Advanced http kernel

--- src/Application.php
+++ src/Application.php
@@ -2,83 +2,8 @@
 
 namespace Pekkis\Phetomane;
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpKernel\HttpKernelInterface;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\RouteCollection;
-use Symfony\Component\Routing\Route;
-use Symfony\Component\Routing\RequestContext;
-use Symfony\Component\Routing\Matcher\UrlMatcher;
-use Symfony\Component\Routing\Exception\ResourceNotFoundException;
-use Symfony\Component\HttpKernel\Controller\ControllerResolver;
-use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
+use Symfony\Component\HttpKernel\HttpKernel;
 
-
-function hello(string $who) {
-    $response = new Response();
-    $response->setContent(sprintf('hello, %s', $who));
-    return $response;
-}
-
-function goodbye() {
-    return Response::create('goodbye :(');
-}
-
-
-class Application implements HttpKernelInterface
+class Application extends HttpKernel
 {
-
-    public function __construct()
-    {
-
-    }
-
-
-    public function handle(Request $request, $type = self::MASTER_REQUEST, $catch = true)
-    {
-        $routes = new RouteCollection();
-        $routes->add(
-            'index',
-            new Route('', [
-                'who' => 'nobody',
-                '_controller' => 'Pekkis\Phetomane\hello'
-            ])
-        );
-        $routes->add(
-            'hello',
-            new Route('hello/{who}', [
-                'who' => 'nobody',
-                '_controller' => 'Pekkis\Phetomane\hello'
-            ])
-        );
-        $routes->add(
-            'goodbye',
-            new Route('goodbye', [
-                '_controller' => 'Pekkis\Phetomane\goodbye'
-            ])
-        );
-
-        $context = new RequestContext();
-        $context->fromRequest($request);
-
-        $matcher = new UrlMatcher($routes, $context);
-
-        try {
-            $match = $matcher->match($request->getPathInfo());
-            $request->attributes->add($match);
-
-            $controllerResolver = new ControllerResolver();
-            $argumentResolver = new ArgumentResolver();
-            $controller = $controllerResolver->getController($request);
-            $arguments = $argumentResolver->getArguments($request, $controller);
-
-            $response = call_user_func_array($controller, $arguments);
-        } catch (ResourceNotFoundException $e) {
-            $response = Response::create('not found')->setStatusCode(404);
-        } catch (\Exception $e) {
-            $response = Response::create('infernal server error')->setStatusCode(404);
-        }
-
-        return $response->prepare($request);
-    }
 }
--- web/index.php
+++ web/index.php
@@ -2,12 +2,80 @@
 
 use Symfony\Component\HttpFoundation\Request;
 use Pekkis\Phetomane\Application;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\RouteCollection;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\HttpKernel\Controller\ControllerResolver;
+use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\EventDispatcher\EventDispatcher;
+use Symfony\Component\HttpKernel\EventListener\RouterListener;
+use Symfony\Component\Debug\Exception\FlattenException;
+use Symfony\Component\HttpKernel\EventListener\ExceptionListener;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+function hello(string $who) {
+    $response = new Response();
+    $response->setContent(sprintf('hello, %s', $who));
+    return $response;
+}
+
+function goodbye() {
+    return Response::create('goodbye :(');
+}
+
 $request = Request::createFromGlobals();
 
-$phetomane = new Application();
+$requestStack = new RequestStack();
+
+$controllerResolver = new ControllerResolver();
+$argumentResolver = new ArgumentResolver();
+
+$routes = new RouteCollection();
+$routes->add(
+    'index',
+    new Route('', [
+        'who' => 'nobody',
+        '_controller' => '\hello'
+    ])
+);
+$routes->add(
+    'hello',
+    new Route('hello/{who}', [
+        'who' => 'nobody',
+        '_controller' => '\hello'
+    ])
+);
+$routes->add(
+    'goodbye',
+    new Route('goodbye', [
+        '_controller' => '\goodbye'
+    ])
+);
+
+$context = new RequestContext();
+$context->fromRequest($request);
+
+$matcher = new UrlMatcher($routes, $context);
+
+$dispatcher = new EventDispatcher();
+$dispatcher->addSubscriber(new RouterListener($matcher, $requestStack));
+
+$errorHandler = function (FlattenException $exception) {
+    $msg = 'Piäleen män!!!! ('.$exception->getMessage().')';
+    return new Response($msg, $exception->getStatusCode());
+};
+$dispatcher->addSubscriber(new ExceptionListener($errorHandler));
+
+$phetomane = new Application(
+    $dispatcher,
+    $controllerResolver,
+    $requestStack,
+    $argumentResolver
+);
 
 $response = $phetomane->handle($request);
 $response->send();
